5193b5350801282d1b5378a930d56f27316f4953
Allampati <Chandrasekhar.Allampati@in.pega.com>
2022-08-02 14:20:24 +0530

    Updated App.js with intial message

--- reactjs-basics/src/App.js
+++ reactjs-basics/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world React.
+          Hello world React learning.
         </p>
         <a
           className="App-link"
