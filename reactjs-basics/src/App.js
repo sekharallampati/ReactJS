c11ea9e1452578ac6708d57710d199b3f1df1a31
Chandrasekhar <sekharallampati@gmail.com>
2022-08-02 14:33:58 +0530

    4th commit

--- reactjs-basics/src/App.js
+++ reactjs-basics/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world React learning.
+          Hello world React learning1.
         </p>
         <a
           className="App-link"
